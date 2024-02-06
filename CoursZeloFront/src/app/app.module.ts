ed7c52e420ef000c44c1f1ddc691be0ef91053f3
Mayahi Nasr <mayahinasr@gmail.com>
2024-02-06 02:12:39 +0100

    Changing Version

--- CoursZeloFront/src/app/app.module.ts
+++ CoursZeloFront/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './Shared/nav/nav.component';
 import { FooterComponent } from './Shared/footer/footer.component';
 import { CourselComponent } from './coursel/coursel.component';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselConfig, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NewCourselComponent } from './new-coursel/new-coursel.component';
 @NgModule({
   declarations: [AppComponent, NavComponent, FooterComponent, CourselComponent],
-  imports: [BrowserModule, AppRoutingModule, NewCourselComponent],
+  imports: [BrowserModule, AppRoutingModule, NewCourselComponent, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
